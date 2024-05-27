bfb4949a6bc86ef39378a8f148e5386fb3d72b0d
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-05-27 18:32:47 +0500

    Day 1 Challenge of 100 Days of Coding

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+// Day 1 Challenge: Start Coding! 
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 1: Install Node.js, TypeScript and VS Code on your Laptop. --- Completed
+//Question 2: Personal Message: Store a person’s name in a variable and print a message to them, 
+// like “Hello Asharib, would you like to learn some TypeScript today?”
+let personName = "Muhammad Saad";
+console.log("Hello " + personName + ", would you like to learn some TypeScript today?");
+//Question 3: Name Cases: Store a person’s name in a variable,
+// and then print that person’s name in lowercase, uppercase, and titlecase.
+let nameOfPerson = "Muhammad Saad";
+// Lower Case -- all small letters
+console.log(nameOfPerson.toLowerCase());
+// Upper Case -- ALL BIG LETTERS
+console.log(nameOfPerson.toUpperCase());
+// Title Case -- Capitalize the first letter of each word
+console.log(nameOfPerson.charAt(0).toUpperCase() + nameOfPerson.slice(1).toLowerCase());
